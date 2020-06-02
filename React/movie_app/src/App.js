12ba29830e4bbefc649b43a6e7189a7a12f1a260
joo-ju <rlawn97@gmail.com>
2020-06-02 16:52:09 +0900

    study React

--- React/movie_app/src/App.js
+++ React/movie_app/src/App.js
@@ -3,13 +3,52 @@ import logo from './logo.svg';
 import './App.css';
 import Movie from './Movie';
 
+// Render : componentWillMount() -> render() -> componentDidMount()
+// Update : componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
+
+const movies = [
+  {
+    title: "Matrix",
+    poster:  "https://cdn.onebauer.media/one/empire-images/articles/5cd5741b133d503e3a49a5c9/matrix-reloaded.jpg?quality=50&width=1800&ratio=16-9&resizeStyle=aspectfill&format=jpg"
+  },
+  {
+    title:  "Full Metal Jacket",
+    poster:   "https://upload.wikimedia.org/wikipedia/en/9/99/Full_Metal_Jacket_poster.jpg"
+  },
+  {
+    title: "Oldboy",
+    poster: "https://upload.wikimedia.org/wikipedia/en/6/67/Oldboykoreanposter.jpg"
+  },
+  {
+    title: "Star Wars",
+    poster: "https://lumiere-a.akamaihd.net/v1/images/star-wars-the-rise-of-skywalker-theatrical-poster-1000_ebc74357.jpeg?region=0%2C0%2C891%2C1372"
+  }
+]
+
+
+const state = {
+  greeting: 'Hello'
+}
+
 function App() {
   return (
     <div className="App">
-      <Movie/>                 {/* 사실 이렇게 id 없이 막 불러 오는건 좋지 않음. */}
+      {state.greeting}
+      {movies.map((movie, index) => {
+        return <Movie title={movie.title} poster={movie.poster} key={index} />
+      })}
+
+      {/* <Movie title={movieTitles[0]} poster={movieImages[0]}/> 
+      <Movie title={movieTitles[1]} poster={movieImages[1]}/> 
+      <Movie title={movieTitles[2]} poster={movieImages[2]}/> 
+      <Movie title={movieTitles[3]} poster={movieImages[3]}/>                 사실 이렇게 id 없이 막 불러 오는건 좋지 않음. */}
 
     </div>
   );
+
+ 
+  
 }
+
 
 export default App;
--- React/movie_app/src/Movie.js
+++ React/movie_app/src/Movie.js
@@ -1,23 +1,36 @@
 import React from 'react';
 import './Movie.css';
+import PropTypes from 'prop-types';
 
 
-function Movie() {
+
+function Movie(props) {
     return (
         <div>
-        <MoviePoster/>
+        <MoviePoster poster={props.poster}/>
         <h1>hello this is a movie</h1>
+        <h1>{props.title}</h1>
         </div>
     );
   }
-
-
+// propTypes를 사용하면 부모 컴포넌트에서 얻는 정보의 종류가 무엇인지, 있는지 없는지 알수 있음.
+Movie.propTypes = {
+  title: PropTypes.string.isRequired,       // string이 아니라 다른 데이터 타입이면 오류 발생
+  poster: PropTypes.string.isRequired       // 즉, 우리가 얻게되는 정보는 숫자가 아니다.
+  // title: PropTypes.string.isRequired 라고 하고 App.js에서 Map()의 title을 없애면 페이지 검사에서 오류가 발생
+} 
   
-function MoviePoster() {
+
+
+
+function MoviePoster(props) {
     return (
-        <img src="http://genomecom.co.kr/wp-content/uploads/2015/05/img28.jpg"/>
-    );
+        <img src={props.poster}/>
+        );
   }
+
+
+
 
 // class Movie extends Component{
 //     render() {
